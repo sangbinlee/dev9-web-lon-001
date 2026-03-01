68af9c7d488fb08b6ce81c31ebd83180684f1120
sangbinlee <sangbinlee9@gmail.com>
2026-03-01 17:11:40 +0900

    경로 정정

--- DB/entrypoint.sh
+++ DB/entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 # WildFly 서버 시작 (백그라운드)
-/opt/wildfly/bin/standalone.sh -b 0.0.0.0 &
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 &
 
 # 서버가 뜰 때까지 대기
 sleep 20
 
 # 데이터소스 추가
-/opt/wildfly/bin/jboss-cli.sh --connect --file=/opt/wildfly/configure-datasource.cli
+/opt/jboss/wildfly/bin/jboss-cli.sh --connect --file=/opt/jboss/wildfly/configure-datasource.cli
 
 # 포그라운드 유지
 fg %1
