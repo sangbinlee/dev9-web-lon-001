9c4040b326cb7e60885a7489ac5bb3de0454836f
sangbinlee <sangbinlee9@gmail.com>
2026-03-02 03:04:20 +0900

    DS TEST 999

--- DB/entrypoint.sh
+++ DB/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# WildFly 실행 백그라운드
-/opt/jboss/wildfly/bin/standalone.sh &
 
-# 서버가 뜰 때까지 대기
+# WildFly 실행 (백그라운드)
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 &
+
+# 서버가 뜰 때까지 대기 (필요시 조정)
 sleep 20
 
-# CLI 스크립트 실행
-/opt/jboss/wildfly/bin/jboss-cli.sh --connect --file=/opt/jboss/wildfly/configure-datasource.cli
+# CLI 스크립트 실행 (서버에 연결)
+ /opt/jboss/wildfly/bin/jboss-cli.sh --connect --file=/opt/jboss/wildfly/configure-datasource.cli
 
-# 포그라운드 실행 유지
-fg
+# WildFly를 포그라운드로 실행 유지
+exec /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0
